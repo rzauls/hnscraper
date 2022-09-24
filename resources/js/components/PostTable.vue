f71b96a6afa46cff7fd8caf92c4286742420bc0e
Rihards Zauls <rihards.zauls@gmail.com>
2022-09-24 22:36:10 +0300

    wire up data-tables

---
+++ resources/js/components/PostTable.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card mt-2">
+        <table id="data-table">
+        </table>
+    </div>
+</template>
+
+<script>
+
+const dt = require('datatables.net');
+
+export default {
+    props: ['posts'],
+    data() {
+        return {
+            table: null,
+            postArray: [],
+        }
+    },
+    mounted() {
+        this.posts.forEach(p => {
+            this.postArray.push([p.title, p.points, p.author, p.link, p.created_at, p.updated_at, "foo"])
+        })
+        this.table = new dt('#data-table', {
+            data: this.postArray,
+            columns: [
+                {title: 'Title'},
+                {title: 'Score'},
+                {title: 'Author'},
+                {title: 'Link'},
+                {title: 'Created At'},
+                {title: 'Last Updated'},
+                {title: 'Buttons'},
+            ]
+        });
+        console.log(this.posts)
+    }
+}
+</script>
+
+
+<style>
+@import 'datatables.net-dt/css/jquery.dataTables.min.css';
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,25 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-                <div id="app">
-                        @foreach($posts as $post)
-                            <example-component :post="{{$post}}"></example-component>
-                        @endforeach
-                </div>
+            <div class="col">
+                <posts-table :posts="{{$posts}}"></posts-table>
             </div>
         </div>
     </div>
